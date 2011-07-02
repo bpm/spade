89d824f4b7ac57f2f8253016067e9d67617c648c
Peter Wagenet <peter.wagenet@gmail.com>
2011-07-01 18:12:45 -0700

    Moved and updated transport for Spade

--- transports/spade.js
+++ transports/spade.js
@@ -1,14 +1,14 @@
 exports.compileTransport = function(code, pkg, id, filename) {
-  var ret = ''
+  var ret = '',
+      id = pkg.name+'/'+id;
 
   // Register package, probably a better way to do this
   if (id.match(/^[^\/]+\/main$/)) {
     ret += 'spade.register("'+pkg.name+'", '+JSON.stringify(pkg)+');\n\n';
   }
 
-  // TODO: This probably isn't quite right
   // TOOD: We can also pass a string here, maybe we should instead
-  ret += 'spade.register("'+id+'", function(){\n'+code+'\n});';
+  ret += 'spade.register("'+id+'", function(require, exports, __module, ARGV, ENV, __filename){\n'+code+'\n});';
 
   return ret;
 };
