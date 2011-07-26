513d63795eeae4ad7f9f49f35b0520de2480e5ce
Charles Jolley <charles@sproutcore.com>
2011-07-25 23:26:22 -0700

    Upgrade spade to work with bpm 1.0.0.beta.7

--- transport.js
+++ transport.js
@@ -1,3 +1,5 @@
+/*globals BPM_PLUGIN */
+
 BPM_PLUGIN.compileTransport = function(code, context, filename) {
   var ret = '',
       packageName = context['package'].name,
@@ -5,11 +7,28 @@ BPM_PLUGIN.compileTransport = function(code, context, filename) {
 
   // Register package, probably a better way to do this
   if (id.match(/^[^\/]+\/main$/)) {
-    ret += 'spade.register("'+packageName+'", '+JSON.stringify(context['package'])+');\n\n';
+    
+    var ctx = context['package'],
+        pkg = { name: ctx.name, 
+                version: ctx.version, 
+                dependencies: ctx.dependencies };
+                
+    ret += 'spade.register("'+packageName+'", '+JSON.stringify(pkg)+');\n\n';
   }
 
-  // TOOD: We can also pass a string here, maybe we should instead
-  ret += 'spade.register("'+id+'", function(require, exports, __module, ARGV, ENV, __filename){\n'+code+'\n});';
+  if (context.settings['spade:format'] === 'function') {
+    code = 'function(require, exports, __module, ARGV, ENV, __filename){\n'+code+'\n}';
+  } else {
+    code = context.minify("(function(require, exports, __module, ARGV, ENV, __filename){\n"+code+"\n//@ sourceURL="+id+"\n})()");
+    
+    var lines = code.split("\n");
+    lines[0] = lines[0].replace(/^\(function\([^\)]+\)\{/, '');
+    lines[lines.length-1] = lines[lines.length-1].replace(/\}\)\(\)$/, '');
+    code = lines.join("\n");
+    code = JSON.stringify(code);
+  }
+
+  ret += 'spade.register("'+id+'", '+code+');';
 
   return ret;
 };
