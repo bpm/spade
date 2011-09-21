54a28986dd4f0a9361a7bfb757095c9402a8f321
Peter Wagenet <peter.wagenet@gmail.com>
2011-09-21 12:35:08 -0700

    Allow 'spade:format': 'none' to prevent wrapping

--- transport.js
+++ transport.js
@@ -1,22 +1,25 @@
 /*globals BPM_PLUGIN */
 
 BPM_PLUGIN.compileTransport = function(code, context, filename) {
+  var format = context.settings['spade:format'];
+  if (format === 'none') { return code };
+
   var ret = '',
       packageName = context['package'].name,
       id = packageName+'/'+context.moduleId;
 
   // Register package, probably a better way to do this
   if (id.match(/^[^\/]+\/main$/)) {
-    
+
     var ctx = context['package'],
         pkg = { name: ctx.name, 
                 version: ctx.version, 
                 dependencies: ctx.dependencies };
-                
+
     ret += 'spade.register("'+packageName+'", '+JSON.stringify(pkg)+');\n\n';
   }
 
-  if (context.settings['spade:format'] === 'function') {
+  if (format === 'function') {
     code = 'function(require, exports, __module, ARGV, ENV, __filename){\n'+code+'\n}';
   } else {
     code = context.minify("(function(require, exports, __module, ARGV, ENV, __filename){"+code+"\n});");
